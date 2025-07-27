01e8e7d5d08e5099b762fe4c81e56ad3261bca32
nathan.carvalho <nathan.carvalho@granadophebo.com.br>
2025-07-27 17:49:10 -0300

    add gcf files that simulate data sources - data sources -> pubsub

---
+++ shopstream/1_api_sources/main.py
@@ -0,0 +1,69 @@
+from flask import jsonify
+import functions_framework
+import logging
+from google.cloud import pubsub_v1
+import os
+
+from .utils import setup_log_execution, get_or_create_pubsub_topic, read_local_file, convert_json_array_to_ndjson
+
+LOG_LEVELS = {"dev": logging.DEBUG, "stg": logging.INFO, "prd": logging.WARNING}
+
+os.environ["PUBSUB_EMULATOR_HOST"] = "localhost:8134"
+
+@functions_framework.http
+def hello_http(request):
+    """
+        HTTP Cloud Function that generates the data simulating the sources.
+    """
+
+    request_json = request.get_json(silent=True)
+
+
+    log_env = request_json.get('log_env', 'dev')
+    source_bucket_name = request_json.get('source_bucket_name', 'your-bucket-name')
+    pubsub_topics = request_json.get('pubsub_topics', {})
+
+
+    assert log_env, "log_env is required"
+    assert source_bucket_name, "source_bucket_name is required"
+    assert pubsub_topics, "pubsub_topics is required"
+
+    setup_log_execution(log_env, LOG_LEVELS)
+
+    try:
+        clickstream_data = read_local_file("clickstream_events.json")
+        customer_support_data = read_local_file("customer_support.json")
+        product_catalog_data = read_local_file("product_catalog.json")
+        sales_transactions_data = read_local_file("transactions.csv")
+
+        data_topic_map = [
+            {"data": clickstream_data, "topic": pubsub_topics.get("clickstream"), "extension": "json"},
+            {"data": customer_support_data, "topic": pubsub_topics.get("customer_support"), "extension": "json"},
+            {"data": product_catalog_data, "topic": pubsub_topics.get("product_catalog"), "extension": "json"},
+            {"data": sales_transactions_data, "topic": pubsub_topics.get("sales_transactions"), "extension": "csv"},
+        ]
+
+        
+        publisher = pubsub_v1.PublisherClient()
+
+        for item in data_topic_map:
+            get_or_create_pubsub_topic(publisher, item["topic"])
+
+        futures = []
+        for item in data_topic_map:
+            logging.info(item)
+            if item["extension"] == "json":
+                item["data"] = convert_json_array_to_ndjson(item["data"])
+                logging.info(item['data'])
+                
+            futures.append(publisher.publish(item["topic"], item["data"]))
+        
+        pubsub_message_ids = [f.result() for f in futures]
+        logging.info(f"Published messages with IDs: {pubsub_message_ids}")
+
+        return jsonify({"message_ids": pubsub_message_ids}), 200
+        
+    except Exception as e:
+        logging.error(f"Failed to publish messages: {e}")
+        raise e
+    
---
+++ shopstream/1_api_sources/utils.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import json
+
+def setup_log_execution(log_env, LOG_LEVELS):
+    log_level = LOG_LEVELS.get(log_env, logging.WARNING)
+    logging.getLogger().setLevel(log_level)
+
+def get_or_create_pubsub_topic(publisher, topic_path):
+    try:
+        publisher.get_topic(request={"topic": topic_path})
+        print(f"Topic {topic_path} already exists.")
+    except Exception:
+        publisher.create_topic(request={"name": topic_path})
+        print(f"Topic {topic_path} created.")
+
+def read_local_file(path):
+    with open(os.path.join("source-data", path), "rb") as f:
+        return f.read()
+
+def convert_json_array_to_ndjson(json_array_string) -> bytes:
+    # Accept bytes or str, always return bytes
+    try:
+        if isinstance(json_array_string, bytes):
+            json_array_string = json_array_string.decode('utf-8')
+        data = json.loads(json_array_string)
+        iterator_of_json_strings = map(json.dumps, data)
+        ndjson_string = "\n".join(iterator_of_json_strings)
+        return ndjson_string.encode('utf-8')
+    except json.JSONDecodeError as e:
+        print(f"Error: Input string is not a valid JSON array. Details: {e}")
+        raise
+    except TypeError as e:
+        print(f"Error: Input must be a string or bytes. Details: {e}")
+        raise
