409437ebadd8af7f6c11a1e1896d472d95034f3a
nathan.carvalho <nathan.carvalho@granadophebo.com.br>
2025-07-30 23:02:15 -0300

    Variable arguments added removing hardcoded values

--- airflow/dags/batch_pipeline.py
+++ airflow/dags/batch_pipeline.py
@@ -1,4 +1,5 @@
 from airflow.decorators import dag, task, task_group
+from airflow.models import Variable
 from airflow.providers.google.cloud.operators.dataform import (
     DataformCreateCompilationResultOperator,
     DataformCreateWorkflowInvocationOperator,
@@ -12,60 +13,25 @@ import logging
 from include.airflow_utils import call_cloud_function
 
 
-"""
-{
-    "dataform": {
-        "project_id": "shopstream-proj", 
-        "repository_id": "shopstream", 
-        "region": "us-west1",
-        "git_commitish": "main"
-    },
-    "gcf": { 
-        "urls": [
-            "https://gcf-shopstream-source-data-gen-274386375025.us-west1.run.app",
-            "https://gcf-pubsub-2-gcs-274386375025.us-central1.run.app"
-        ],
-        "args": {
-            "simulate_data_source": "...",
-            "pubsub_2_gcs": "..."
-        }
-    },
-    "airflow": {
-        "default_args": {
-            "owner": "ShopStream DE Team",
-            "depends_on_past": false,
-            "retries": 3,
-            "retry_delay_time": 30,
-            "sla_time": 24
-            }
-    }
-}
-"""
+config_params = Variable.get("AIRFLOW_SHOPSTREAM", deserialize_json=True)
 
-PROJECT_ID = "shopstream-proj"
-REPOSITORY_ID = "shopstream"
-REGION = "us-west1"
-GIT_COMMITISH = "main"
+PROJECT_ID = config_params["dataform"]["project_id"]
+REPOSITORY_ID = config_params["dataform"]["repository_id"]
+REGION = config_params["dataform"]["region"]
+GIT_COMMITISH = config_params["dataform"]["git_commitish"]
 
-URL_GCF = [
-    "https://gcf-shopstream-source-data-gen-274386375025.us-west1.run.app",
-    "https://gcf-pubsub-2-gcs-274386375025.us-central1.run.app"
-]
-
-ARG_GCF_SIMULATE_DATA_SOURCE = {
-    "simulate_data_source": ""# TODO: add as parameter
-}
-ARG_GCF_PUBSUB_2_GCS = {
-    "pubsub_2_gcs": ""# TODO: add as parameter
-}
+URL_GCF_DATA_SOURCE = config_params["gcf"]["urls"]["data_source"]
+URL_GCF_PUBSUB_2_GCS = config_params["gcf"]["urls"]["pubsub_2_gcs"]
+ARG_GCF_SIMULATE_DATA_SOURCE = config_params["gcf"]["args"]["simulate_data_source"]
+ARG_GCF_PUBSUB_2_GCS = config_params["gcf"]["args"]["pubsub_2_gcs"]
 
 
 default_args = dict(
-    owner="ShopStream DE Team",
-    depends_on_past=False,
-    retries=3,
-    retry_delay=timedelta(minutes=30),
-    sla=timedelta(hours=24)
+    owner=config_params["airflow"]["default_args"]["owner"],
+    depends_on_past=False,  
+    retries=config_params["airflow"]["default_args"]["retries"],
+    retry_delay=timedelta(minutes=config_params["airflow"]["default_args"]["retry_delay_time"]),
+    sla=timedelta(hours=config_params["airflow"]["default_args"]["sla_time"])
 )
 
 local_tz = pendulum.timezone("America/Sao_Paulo")
@@ -73,7 +39,7 @@ logger = logging.getLogger('airflow.task')
 
 
 @dag(
-    'execute_shopstream',
+    'execute_shopstream_pipe',
     start_date=datetime(2025, 5, 5),
     schedule="30 3 * * 1",
     catchup=False,
@@ -87,27 +53,36 @@ logger = logging.getLogger('airflow.task')
     tags=['shopstream', 'batch', 'daily']
 )
 def shopstream_pipeline():
+    logger.info("Starting shopstream_pipeline DAG definition...")
 
+    logger.info("Initializing start and end tasks for DAG...")
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
 
     @task_group(group_id="ingestion_to_raw")
     def ingestion_to_raw():
+        logger.info("Starting ingestion_to_raw task group...")
         @task(task_id="gcf-simulate-data-source")
         def gcf_simulate_data_source():
+            logger.info("Starting gcf_simulate_data_source task...")
+            logger.debug(f"Calling cloud function at {URL_GCF_DATA_SOURCE} with args: {ARG_GCF_SIMULATE_DATA_SOURCE}")
             response = call_cloud_function(
-                URL_GCF, 
+                URL_GCF_DATA_SOURCE, 
                 ARG_GCF_SIMULATE_DATA_SOURCE
             )
+            logger.info(f"Received response from gcf_simulate_data_source: {response}")
             return response
 
 
         @task(task_id="gcf-pubsub-2-gcs")
         def gcf_pubsub_2_gcs():
+            logger.info("Starting gcf_pubsub_2_gcs task...")
+            logger.debug(f"Calling cloud function at {URL_GCF_PUBSUB_2_GCS} with args: {ARG_GCF_PUBSUB_2_GCS}")
             response = call_cloud_function(
-                URL_GCF, 
+                URL_GCF_PUBSUB_2_GCS, 
                 ARG_GCF_PUBSUB_2_GCS
             )
+            logger.info(f"Received response from gcf_pubsub_2_gcs: {response}")
             return response
 
         gcf_simulate_data_source() >> gcf_pubsub_2_gcs()
@@ -115,6 +90,7 @@ def shopstream_pipeline():
 
     @task_group(group_id="transform_to_trusted")
     def transform_to_trusted():
+        logger.info("Starting transform_to_trusted task group...")
         dataform_compilation_result = DataformCreateCompilationResultOperator(
             task_id="create_compilation_result",
             project_id=PROJECT_ID,
